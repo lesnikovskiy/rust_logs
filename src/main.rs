41ffeb59f566e70d9df078e079849f57063b33c7
lesnikovskiy <lesnikovski@gmail.com>
2024-08-04 23:53:56 +0300

    Handle exceptions demo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::fs;
+use std::io::Error;
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
+    let text = fs::read_to_string("logs.txt").unwrap();
     println!("{:#?}", text);
+
+    match divide(5.0, 3.0) {
+        Ok(result_of_devision) => println!("{}", result_of_devision),
+        Err(what_went_wrong) => println!("{}", what_went_wrong),
+    }
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 { Err(Error::other("can't divide by 0")) } else { Ok(a / b) }
 }

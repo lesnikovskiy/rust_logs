6dfe2842c9beccc5b3787fc3cddfa0ca6d819b3a
lesnikovskiy <lesnikovski@gmail.com>
2024-08-05 00:04:12 +0300

    validating data which return nothing with empty tuple

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,15 @@ fn main() {
         Ok(result_of_devision) => println!("{}", result_of_devision),
         Err(what_went_wrong) => println!("{}", what_went_wrong),
     }
+
+    match validate_email(String::from("lekovskigmail.com")) {
+        Ok(..) => println!("Email valid"),
+        Err(what_is_wrong) => println!("{}", what_is_wrong),
+    }
+}
+
+fn validate_email(email: String) -> Result<(), Error> {
+    if email.contains("@") { Ok(()) } else { Err(Error::other("emails must have an @")) }
 }
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {

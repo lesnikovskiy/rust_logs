d5a1ed8a0439b639ee2aa190b9713dcaeac37638
lesnikovskiy <lesnikovski@gmail.com>
2024-08-05 23:27:14 +0300

    Three types of Strings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::fs;
 
+fn string_test(a: String, b: &String, c: &str) {}
+
 fn main() {
+    string_test(String::from("red"), &String::from("red"), "red");
+    string_test("red".to_string(), &"red".to_string(), "red");
+
     match fs::read_to_string("logs.txt") {
-        Ok(text) => println!("{}", text),
-        Err(error) => println!("Error occurred {}", error),
+        Ok(text_that_was_read) => { println!("{}", text_that_was_read.len()) }
+        Err(why_this_failed) => { println!("Failed to read file: {}", why_this_failed) }
     }
 }

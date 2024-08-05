93f4bdcca3357cbbf080b59317f5be31ead22161
lesnikovskiy <lesnikovski@gmail.com>
2024-08-05 23:46:12 +0300

    Creating &str from String

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ fn main() {
     string_test(String::from("red"), &String::from("red"), "red");
     string_test("red".to_string(), &"red".to_string(), "red");
 
-    // Creating slice string
-    let color = String::from("red");
-    let c = color.as_str();
+    // Creating &str
+    let color = String::from("Bluetti");
+    let portion = &color[0..4];
+    println!("{}", portion);
 
     match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) => { println!("{}", text_that_was_read.len()) }

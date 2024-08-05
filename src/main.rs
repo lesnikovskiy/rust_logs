7b91f1043635f2834f931c751f27c4e6cf9a3794
lesnikovskiy <lesnikovski@gmail.com>
2024-08-05 23:38:25 +0300

    Creating slice string

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     string_test(String::from("red"), &String::from("red"), "red");
     string_test("red".to_string(), &"red".to_string(), "red");
 
+    // Creating slice string
+    let color = String::from("red");
+    let c = color.as_str();
+
     match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) => { println!("{}", text_that_was_read.len()) }
         Err(why_this_failed) => { println!("Failed to read file: {}", why_this_failed) }

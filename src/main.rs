83a6645058926fd3a2e0022011f6b331f803d136
lesnikovskiy <lesnikovski@gmail.com>
2024-08-06 00:03:18 +0300

    excercise in extracting error lines from logs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use std::fs;
 
-fn string_test(a: String, b: &String, c: &str) {}
+fn extract_errors(log: &str) -> Vec<&str> {
+    let lines = log.split("\n");
+    let mut vec = vec![];
+
+    for line in lines {
+        if line.starts_with("ERROR") {
+            vec.push(line);
+        }
+    }
+
+    vec
+}
 
 fn main() {
-    string_test(String::from("red"), &String::from("red"), "red");
-    string_test("red".to_string(), &"red".to_string(), "red");
-
-    // Creating &str
-    let color = String::from("Bluetti");
-    let portion = &color[0..4];
-    println!("{}", portion);
-
     match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => { println!("{}", text_that_was_read.len()) }
+        Ok(text) => {
+            let lines = extract_errors(&text);
+            lines.iter().for_each(|line| println!("{}", line))
+        }
         Err(why_this_failed) => { println!("Failed to read file: {}", why_this_failed) }
     }
 }

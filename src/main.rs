240e982670c1530fd6e81ea1e8dedb4f89b1b299
lesnikovskiy <lesnikovski@gmail.com>
2024-08-06 23:01:32 +0300

    Use expect instead of nested matching

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,22 @@ fn extract_errors(text: &str) -> Vec<&str> {
 }
 
 fn main() {
-    match fs::read_to_string("logs.txt") {
-        Ok(text) => {
-            let error_logs = extract_errors(text.as_str());
-            println!("{:#?}", error_logs);
-            match fs::write("errors.txt", error_logs.join("\n")) {
-                Ok(..) => println!("Wrote errors.txt"),
-                Err(reason_write_failed) =>
-                    println!("Writing of errors.txt failed: {}", reason_write_failed),
-            }
-        }
-        Err(why_this_failed) => {
-            println!("Failed to read file: {}", why_this_failed);
-        }
-    }
+    let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+    let error_logs = extract_errors(&text);
+    fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
+
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text) => {
+    //         let error_logs = extract_errors(text.as_str());
+
+    //         match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => println!("Wrote errors.txt"),
+    //             Err(reason_write_failed) =>
+    //                 println!("Writing of errors.txt failed: {}", reason_write_failed),
+    //         }
+    //     }
+    //     Err(why_this_failed) => {
+    //         println!("Failed to read file: {}", why_this_failed);
+    //     }
+    // }
 }

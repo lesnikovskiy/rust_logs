e1f74ff3598178ededb915e48f8051d0830d2f37
lesnikovskiy <lesnikovski@gmail.com>
2024-08-06 22:53:30 +0300

    Writing to file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::fs;
 
-fn extract_errors(text: &str) -> Vec<String> {
+fn extract_errors(text: &str) -> Vec<&str> {
     let split_text = text.split("\n");
 
     let mut results = vec![];
 
     for line in split_text {
         if line.starts_with("ERROR") {
-            results.push(line.to_string());
+            results.push(line);
         }
     }
 
@@ -15,16 +15,18 @@ fn extract_errors(text: &str) -> Vec<String> {
 }
 
 fn main() {
-    let mut error_logs = vec![];
-
     match fs::read_to_string("logs.txt") {
         Ok(text) => {
-            error_logs = extract_errors(text.as_str());
+            let error_logs = extract_errors(text.as_str());
+            println!("{:#?}", error_logs);
+            match fs::write("errors.txt", error_logs.join("\n")) {
+                Ok(..) => println!("Wrote errors.txt"),
+                Err(reason_write_failed) =>
+                    println!("Writing of errors.txt failed: {}", reason_write_failed),
+            }
         }
         Err(why_this_failed) => {
             println!("Failed to read file: {}", why_this_failed);
         }
     }
-
-    println!("{:#?}", error_logs);
 }
